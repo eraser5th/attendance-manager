6e6bc4ee7e17b8f5b95cb21c0071f60afa6b81bf
eraser5th <83107074+eraser5th@users.noreply.github.com>
2024-04-13 15:57:30 +0900

    linebotうごいたぁあぁあ

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use actix_web::{get, web::ServiceConfig};
 use shuttle_actix_web::ShuttleActixWeb;
+use shuttle_runtime::SecretStore;
+
+use std::sync::Arc;
 
 use actix_web::{error::ErrorBadRequest, post, web, Error, HttpResponse};
 use dotenv::dotenv;
@@ -13,15 +16,18 @@ use line_bot_sdk_rust::{
     parser::signature::validate_signature,
     support::actix::Signature,
 };
-use std::env;
+
+#[derive(Clone)]
+struct AppState {
+    line_channel_secret: Arc<String>,
+    line_channel_access_token: Arc<String>,
+}
 
 #[post("/callback")]
-async fn callback(signature: Signature, bytes: web::Bytes) -> Result<HttpResponse, Error> {
+async fn callback(data: web::Data<AppState>, signature: Signature, bytes: web::Bytes) -> Result<HttpResponse, Error> {
     // Get channel secret and access token by environment variable
-    let channel_secret: &str =
-        &env::var("LINE_CHANNEL_SECRET").expect("Failed to get LINE_CHANNEL_SECRET");
-    let access_token: &str =
-        &env::var("LINE_CHANNEL_ACCESS_TOKEN").expect("Failed to get LINE_CHANNEL_ACCESS_TOKEN");
+    let channel_secret = data.line_channel_secret.get(0..).unwrap();
+    let access_token = data.line_channel_access_token.get(0..).unwrap();
 
     let line = LINE::new(access_token.to_string());
 
@@ -68,10 +74,21 @@ async fn hello_world() -> &'static str {
 }
 
 #[shuttle_runtime::main]
-async fn main() -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
+async fn main(
+    #[shuttle_runtime::Secrets] secrets: SecretStore,
+) -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
+    let channel_secret = secrets.get("LINE_CHANNEL_SECRET").expect("Failed to get LINE_CHANNEL_SECRET");
+    let access_token = secrets.get("LINE_CHANNEL_ACCESS_TOKEN").expect("Failed to get LINE_CHANNEL_ACCESS_TOKEN");
+    let app_state = AppState {
+        line_channel_secret: Arc::new(channel_secret),
+        line_channel_access_token: Arc::new(access_token),
+    };
+
     dotenv().ok();
     let config = move |cfg: &mut ServiceConfig| {
-        cfg.service(callback);
+        cfg
+            .app_data(web::Data::new(app_state.clone()))
+            .service(callback);
     };
 
     Ok(config.into())
